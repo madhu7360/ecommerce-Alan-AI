9de117de393fe12a69976f60ea0aef40ffdd54fe
madhu7360 <m.mali@iitg.ac.in>
2023-08-08 03:28:57 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import { CartProvider } from "./context/CartContext"
+//import "tailwindcss/tailwind.css"
+import './style.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+)
